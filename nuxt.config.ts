c69221e3e2d42cee236172737ce4894e8cd720e0
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2025-01-27 20:44:21 +0900

    update: restructure README and migrate configuration to new format

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      catApiKey: process.env.CAT_API_KEY
+    }
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/i18n', '@nuxt/icon', '@nuxt/test-utils/module'],
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+    vueI18n: './i18n/i18n.config.ts'
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic:wght@400;700&display=swap'
+        }
+      ]
+    }
+  }
+})
