e3b05844b3bb68a3aeab5796156badb05fed31c3
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2025-02-02 19:45:29 +0900

    feat: add additional image positions for new cat breeds

--- assets/data/imagePositions.ts
+++ assets/data/imagePositions.ts
@@ -1,3 +1,10 @@
 export const imagePositions: Record<string, string> = {
   'beng': 'right center',
+  'ocic': 'right center',
+  'orie': 'top center',
+  'awir': 'left center',
+  'aege': 'left center',
+  'sibe': 'left center',
+  'bure': 'left center',
+  'ycho': 'top center',
 } 
